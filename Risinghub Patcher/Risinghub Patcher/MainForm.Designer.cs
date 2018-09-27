333124b05f5bfa069a5f0c03ccf3c9ce009b3fc7
Jannik P <30756486+Tempodoa@users.noreply.github.com>
2018-09-27 19:23:50 +0200

    Initial Commit

---
+++ Risinghub Patcher/Risinghub Patcher/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Risinghub_Patcher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.DLPathBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.browseDLPath = new System.Windows.Forms.Button();
+            this.chooseDestButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DestinationBox = new System.Windows.Forms.TextBox();
+            this.patchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DLPathBox
+            // 
+            this.DLPathBox.Location = new System.Drawing.Point(121, 12);
+            this.DLPathBox.Name = "DLPathBox";
+            this.DLPathBox.Size = new System.Drawing.Size(437, 20);
+            this.DLPathBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DarkLauncher Path:";
+            // 
+            // browseDLPath
+            // 
+            this.browseDLPath.Location = new System.Drawing.Point(564, 10);
+            this.browseDLPath.Name = "browseDLPath";
+            this.browseDLPath.Size = new System.Drawing.Size(75, 23);
+            this.browseDLPath.TabIndex = 2;
+            this.browseDLPath.Text = "Browse";
+            this.browseDLPath.UseVisualStyleBackColor = true;
+            this.browseDLPath.Click += new System.EventHandler(this.browseDLPath_Click);
+            // 
+            // chooseDestButton
+            // 
+            this.chooseDestButton.Location = new System.Drawing.Point(564, 36);
+            this.chooseDestButton.Name = "chooseDestButton";
+            this.chooseDestButton.Size = new System.Drawing.Size(75, 23);
+            this.chooseDestButton.TabIndex = 5;
+            this.chooseDestButton.Text = "Browse";
+            this.chooseDestButton.UseVisualStyleBackColor = true;
+            this.chooseDestButton.Click += new System.EventHandler(this.chooseDestButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination Path:";
+            // 
+            // DestinationBox
+            // 
+            this.DestinationBox.Location = new System.Drawing.Point(121, 38);
+            this.DestinationBox.Name = "DestinationBox";
+            this.DestinationBox.Size = new System.Drawing.Size(437, 20);
+            this.DestinationBox.TabIndex = 3;
+            // 
+            // patchButton
+            // 
+            this.patchButton.Location = new System.Drawing.Point(215, 64);
+            this.patchButton.Name = "patchButton";
+            this.patchButton.Size = new System.Drawing.Size(223, 42);
+            this.patchButton.TabIndex = 6;
+            this.patchButton.Text = "Patch";
+            this.patchButton.UseVisualStyleBackColor = true;
+            this.patchButton.Click += new System.EventHandler(this.patchButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 117);
+            this.Controls.Add(this.patchButton);
+            this.Controls.Add(this.chooseDestButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DestinationBox);
+            this.Controls.Add(this.browseDLPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DLPathBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "RisingHub Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DLPathBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button browseDLPath;
+        private System.Windows.Forms.Button chooseDestButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DestinationBox;
+        private System.Windows.Forms.Button patchButton;
+    }
+}
+
---
+++ Risinghub Patcher/Risinghub Patcher/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+
+namespace Risinghub_Patcher
+{
+    public partial class MainForm : Form
+    {
+        private string DownloadsPath;
+        private string DesktopPath;
+        private Random random;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            random = new Random();
+            DownloadsPath = KnownFolders.GetPath(KnownFolder.Downloads);
+            DesktopPath = KnownFolders.GetPath(KnownFolder.Desktop);
+
+            var DLFiles = Directory.GetFiles(DownloadsPath, "DarkLauncher.exe", SearchOption.AllDirectories);
+            var DesktopFiles = Directory.GetFiles(DesktopPath, "DarkLauncher.exe", SearchOption.AllDirectories);
+            string[] allFiles = DLFiles.Concat(DesktopFiles).ToArray();
+            if (allFiles.Length == 0)
+                DLPathBox.Text = "Browse and select DarkLauncher.exe";
+            else
+                DLPathBox.Text = allFiles[0];
+
+            DestinationBox.Text = Path.Combine(DesktopPath, "DarkLauncher (Modified)");
+        }
+
+        private void browseDLPath_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = DownloadsPath;
+                openFileDialog1.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    DLPathBox.Text = openFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void chooseDestButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderDialog.ShowDialog();
+                if(result== DialogResult.OK)
+                {
+                    DestinationBox.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void patchButton_Click(object sender, EventArgs e)
+        {
+            if(!Directory.Exists(DestinationBox.Text))
+                Directory.CreateDirectory(DestinationBox.Text);
+            if (!File.Exists(DLPathBox.Text))
+            {
+                MessageBox.Show("Couldn't find specified DarkLauncher.exe");
+                return;
+            }
+                
+            var assembly = AssemblyDefinition.ReadAssembly(DLPathBox.Text);
+            var programType = assembly.MainModule.Types.First(x => x.Name == "HWID");
+            var mainMethod = programType.Methods.First(x => x.Name == "getHard");
+            var FileReadMethod = typeof(File).GetMethod("ReadAllText", new Type[] { typeof(string) });
+            var FileReadRef = assembly.MainModule.ImportReference(FileReadMethod);
+            mainMethod.Body.Instructions.Insert(74, Instruction.Create(OpCodes.Ldstr, "hwid.txt"));
+            mainMethod.Body.Instructions.Insert(75, Instruction.Create(OpCodes.Call, FileReadRef));
+            assembly.Write(Path.Combine(DestinationBox.Text, "DarkLauncher (Modified).exe"));
+            File.WriteAllText(Path.Combine(DestinationBox.Text, "hwid.txt"),RandomString(30));
+
+            MessageBox.Show("Sucessfully modified your DarkLauncher.\n\nIMPORTANT:\n1: Change the text inside hwid.txt after getting banned again.\n2: USE A MAC ADDRESS CHANGER (i.e. TMAC/MyMAC)");
+        }
+
+        private string RandomString(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
